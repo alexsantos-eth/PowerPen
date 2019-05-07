51abb6c5a19abeb23f178b1f4ffaa52daeb2185c
Alex <Alex>
2019-05-07 06:16:29 -0600

    Add materialize

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
 const CACHE_NAME = "v1_cache";
 const urlsToCache=[
   "./",
-  "./main.js", 
-  "./img/icon.png"
+  "./css/style.css",
+  "./js/main.js", 
+  "./img/icon.png", 
+  "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js", 
+  "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css", 
+  "https://cdn.jsdelivr.net/gh/blobysoftware/GeckoJs/gecko.min.js"
 ]
 
 self.addEventListener("install", e => {
@@ -24,29 +28,23 @@ self.addEventListener('activate', e => {
       .then(cacheNames => {
         return Promise.all(
           cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
             if (cacheWhitelist.indexOf(cacheName) === -1) {
               return caches.delete(cacheName)
             }
           })
         )
       })
-    // Le indica al SW activar el cache actual
       .then(() => self.clients.claim())
   )
 })
 
-//cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
     caches.match(e.request)
       .then(res => {
         if (res) {
-          //recuperar del cache
           return res
         }
-        //recuperar de la petición a la url
         return fetch(e.request)
       })
   )
