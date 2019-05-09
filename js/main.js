5ccb8fe65dd3ea2509b77b4f5f604a086a3b323c
Alex <Alex>
2019-05-08 19:25:32 -0600

    Fix push

--- js/main.js
+++ js/main.js
@@ -38,28 +38,10 @@ Notification.requestPermission(function(status) {
 });
 
 function notifyMe(msg) {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    console.log("This browser does not support desktop notification");
-  }
-  
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification(msg);
-  }
-  
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification(msg);
-      }
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification(msg);
     });
   }
-  
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
 }
 notifyMe("hola");
