a44aca4b784f166b5fb2d876ac8b80aec3e3c023
Alex <Alex>
2019-05-08 18:34:41 -0600

    Fix all

--- sw.js
+++ sw.js
@@ -7,10 +7,10 @@ const urlsToCache=[
   "./img/logoP.png",
   "./img/classroom2.jpg",
   "./img/user2.jpg", 
-  "./img/banner.jpg", 
   "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js", 
   "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css", 
-  "https://fonts.googleapis.com/icon?family=Material+Icons"
+  "https://fonts.googleapis.com/icon?family=Material+Icons", 
+  "https://fonts.googleapis.com/css?family=Poppins"
 ]
 
 self.addEventListener("install", e => {
