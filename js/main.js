afd6ab1b546ad7f847e1f2a077c1037256756838
Alex <Alex>
2019-05-08 19:11:05 -0600

    Test notifications

--- js/main.js
+++ js/main.js
@@ -31,3 +31,17 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 var elems = document.querySelectorAll('.sidenav');
 var instances = M.Sidenav.init(elems, {preventScrolling:true}); 
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
+
+displayNotification();
